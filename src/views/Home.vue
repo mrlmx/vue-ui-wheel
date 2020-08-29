04c68a1e4e7995dd5472ae799820d0b68b43f1b3
mrlmx <906529775@qq.com>
2020-08-29 14:43:45 +0800

    update top-nav style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,40 @@
 <template>
-    <div>
-        <div class="top-nav">
-            <div class="logo">Logo</div>
-            <div class="mebu">Menu</div>
-        </div>
-        <div class="banner">
-            <h1>Vue  UI Wheel</h1>
-            <h2>a vue ui component project for practicing and it's just a toy</h2>
-            <p class="action">
-                <a>GitHub</a>
-                <a>Start</a>
-            </p>
-        </div>
+<div>
+    <div class="top-nav">
+        <div class="logo">LOGO</div>
+        <ul class="menu">
+            <li>菜单1</li>
+            <li>菜单2</li>
+        </ul>
     </div>
+    <div class="banner">
+        <h1>Vue UI Wheel</h1>
+        <h2>a vue ui component project for practicing and it's just a toy</h2>
+        <p class="action">
+            <a>GitHub</a>
+            <a>Start</a>
+        </p>
+    </div>
+</div>
 </template>
 
 <style lang="scss" scoped>
-.top-nav{
-    border: 1px solid red;
+.top-nav {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: 15px 20px;
+    background: #EFEFEF;
+
+    .logo {
+        font-size: 22px;
+    }
+
+    .menu {
+        li {
+            display: inline-block;
+            margin-left: 20px;
+        }
+    }
 }
 </style>
