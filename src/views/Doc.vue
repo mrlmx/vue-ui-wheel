2fb4dfc242538bd83fcc5c554c3862d1b2c1420d
mrlmx <906529775@qq.com>
2020-08-29 14:52:29 +0800

    create topnav component

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,12 +1,18 @@
 <template>
-    <div>
-        <div class="top-nav">
-            <div class="logo">Logo</div>
-            <div class="mebu">Menu</div>
-        </div>
-        <div class="content">
-            <aside>aside</aside>
-            <main>main</main>
-        </div>
+<div>
+    <Topnav />
+    <div class="content">
+        <aside>aside</aside>
+        <main>main</main>
     </div>
+</div>
 </template>
+
+<script lang="ts">
+import Topnav from "../components/Topnav.vue";
+export default {
+    components: {
+        Topnav,
+    },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
 <div>
-    <div class="top-nav">
-        <div class="logo">LOGO</div>
-        <ul class="menu">
-            <li>菜单1</li>
-            <li>菜单2</li>
-        </ul>
-    </div>
+    <Topnav />
     <div class="banner">
         <h1>Vue UI Wheel</h1>
         <h2>a vue ui component project for practicing and it's just a toy</h2>
@@ -18,23 +12,14 @@
 </div>
 </template>
 
+<script lang="ts">
+import Topnav from "../components/Topnav.vue";
+export default {
+    components: {
+        Topnav,
+    },
+};
+</script>
+
 <style lang="scss" scoped>
-.top-nav {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    padding: 15px 20px;
-    background: #EFEFEF;
-
-    .logo {
-        font-size: 22px;
-    }
-
-    .menu {
-        li {
-            display: inline-block;
-            margin-left: 20px;
-        }
-    }
-}
 </style>
