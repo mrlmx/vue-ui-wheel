de13abe3a28cfa77b7580f82c3451feed9b7d5ca
mrlmx <906529775@qq.com>
2020-08-29 15:39:17 +0800

    update doc page's aside

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,8 +2,24 @@
 <div>
     <Topnav />
     <div class="content">
-        <aside>aside</aside>
-        <main>main</main>
+        <aside class="aside">
+            <h2>组件列表</h2>
+            <ol>
+                <li>
+                    <router-link to="/doc/switch">Switch 组件</router-link>
+                </li>
+                <li>
+                    <router-link to="/doc/button">Button 组件</router-link>
+                </li>
+                <li>
+                    <router-link to="dialog">Dialog 组件</router-link>
+                </li>
+                <li>
+                    <router-link to="/doc/tabs">Tabs 组件</router-link>
+                </li>
+            </ol>
+        </aside>
+        <main class="main">main</main>
     </div>
 </div>
 </template>
@@ -16,3 +32,30 @@ export default {
     },
 };
 </script>
+
+<style lang="scss" scoped>
+.content {
+    display: flex;
+    height: calc(100vh - 56px);
+
+    .aside {
+        width: 150px;
+        flex-shrink: 0;
+        padding: 20px;
+        border-right: 1px solid #666;
+
+        h2 {
+            margin-bottom: 15px;
+        }
+
+        li {
+            margin: 10px 0;
+        }
+    }
+
+    .main {
+        flex-grow: 1;
+        padding: 20px;
+    }
+}
+</style>
