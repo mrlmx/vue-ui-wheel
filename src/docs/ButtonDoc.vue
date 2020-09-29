65e3864d989efc5ef8023fc43b182b15918dba89
mrlmx <906529775@qq.com>
2020-09-30 00:19:46 +0800

    add button class

--- src/docs/ButtonDoc.vue
+++ src/docs/ButtonDoc.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <Button @click="handleClick">按钮</Button>
+    <div class="btn-wrapper">
+        <Button>按钮</Button>
+        <Button type="primary">按钮</Button>
+        <Button type="ghost">按钮</Button>
+        <Button type="dashed">按钮</Button>
+    </div>
     </div>
 </template>
 
@@ -20,4 +25,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.btn-wrapper{
+    button{
+        margin: 0 20px 20px 0;
+    }
+}
 </style>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,24 +1,63 @@
 <template>
-    <div>
-        <button v-bind="rest">
-            <slot></slot>
-        </button>
-    </div>
+    <button class="xin-button" :class="{ [`xin-button-${type}`]: type }">
+        <slot></slot>
+    </button>
 </template>
 
 <script>
 export default {
     inheritAttrs: false,
-    setup(props, ctx){
-        const {size, ...rest} = ctx.attrs;
+    props: {
+        // type: {
+        //     type: String,
+        //     default: "default",
+        // },
+        type: String,
+    },
+    setup(props, ctx) {
+        const { size, ...rest } = ctx.attrs;
         return {
             size,
-            rest
-        }
-    }
+            rest,
+        };
+    },
 };
 </script>
 
 <style lang="scss" scoped>
+$color: #40a9ff;
+$height: 32px;
+$border-radius: 2px;
 
+.xin-button {
+    display: inline-block;
+    font-weight: 400;
+    white-space: nowrap;
+    text-align: center;
+    background-image: none;
+    box-shadow: 0 2px 0 rgba(0, 0, 0, 0.015);
+    cursor: pointer;
+    transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+    user-select: none;
+    touch-action: manipulation;
+    height: $height;
+    padding: 4px 15px;
+    font-size: 14px;
+    border-radius: $border-radius;
+    color: rgba(0, 0, 0, 0.85);
+    background: #fff;
+    border: 1px solid #d9d9d9;
+
+    &:active,
+    &:focus {
+        outline: 0;
+    }
+
+    &:focus,
+    &:hover {
+        color: $color;
+        background: #fff;
+        border-color: $color;
+    }
+}
 </style>
