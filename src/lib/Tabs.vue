aa0ba6615f1a0a1b03093773f6efade909c3066d
mrlmx <906529775@qq.com>
2020-10-29 23:37:22 +0800

    optimize Tab component code

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -4,12 +4,12 @@
             <div
                 class="xin-tabs-nav-item"
                 :class="{ 'xin-tabs-nav-active': active === tab.key }"
-                v-for="(tab, i) of tabProps"
+                v-for="tab of tabProps"
                 :key="tab.key"
                 @click="handleTabClick(tab.key)"
                 :ref="
                     (el) => {
-                        if (el) navItems[i] = el;
+                        if (active === tab.key) activedNav = el;
                     }
                 "
             >
@@ -24,14 +24,14 @@
 </template>
 
 <script lang="ts">
-import { computed, onMounted, onUpdated, ref } from "vue";
+import { computed, ref, watchEffect } from "vue";
 import Tab from "./Tab.vue";
 export default {
     props: {
         active: String,
     },
     setup(props, context) {
-        const navItems = ref<HTMLDivElement[]>([]);
+        const activedNav = ref<HTMLDivElement>(null);
         const navWrapper = ref<HTMLDivElement>(null);
         const navLine = ref<HTMLDivElement>(null);
         const defaultSlot = context.slots.default();
@@ -46,28 +46,14 @@ export default {
                 (tab) => tab.props.key === props.active
             )[0];
         });
-        onMounted(() => {
-            const activeNav = navItems.value.filter((nav) => {
-                return nav.classList.contains("xin-tabs-nav-active");
-            })[0];
-            const { width, left } = activeNav.getBoundingClientRect();
+        watchEffect(() => {
+            const { width, left } = activedNav.value.getBoundingClientRect();
             navLine.value.style.width = width + "px";
-            const navWrapperPostion = navWrapper.value.getBoundingClientRect();
-            navLine.value.style.left = left - navWrapperPostion.left + "px";
+            const navWrapperPosition = navWrapper.value.getBoundingClientRect();
+            navLine.value.style.left = left - navWrapperPosition.left + "px";
         });
-        onUpdated(() =>{
-            const activeNav = navItems.value.filter((nav) => {
-                return nav.classList.contains("xin-tabs-nav-active");
-            })[0];
-            const { width, left } = activeNav.getBoundingClientRect();
-            navLine.value.style.width = width + "px";
-            const navWrapperPostion = navWrapper.value.getBoundingClientRect();
-            navLine.value.style.left = left - navWrapperPostion.left + "px";
-        })
         const handleTabClick = (key) => {
-            console.log(key);
             context.emit("update:active", key);
-            // activeTab.value = index;
         };
         return {
             defaultSlot,
@@ -75,7 +61,7 @@ export default {
             // activeTab,
             handleTabClick,
             tabComponent,
-            navItems,
+            activedNav,
             navLine,
             navWrapper,
         };
